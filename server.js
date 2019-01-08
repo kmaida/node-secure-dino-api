015f1084f4f54ce6c1905f7e7adda8ca382e583a
Kim Maida <kim.maida@auth0.com>
2019-01-08 17:25:17 -0500

    Remove unneeded line in server

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
